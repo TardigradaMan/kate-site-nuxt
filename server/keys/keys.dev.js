2ce8bba76a3cbc4e2245050167111ed12b027894
Fadin Ivan <fadin89@gmail.com>
2020-07-23 15:01:47 +0700

    Редактирование gitignore

--- server/keys/keys.dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  // MONGO_URI: `mongodb://localhost:27017/kate`,
-  MONGO_URI: `mongodb+srv://kite_site:lKVU93tSgKSaaf3t@cluster0.hdavq.mongodb.net/kite-site?retryWrites=true&w=majority`,
-  JWT: 'dev-jwt-key',
-  TOKEN: '1009703975:AAFUGJCiXhNZF3vfTfG5787o1Fx4q7jtuRc'
-}
